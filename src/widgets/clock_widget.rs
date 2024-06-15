1ea7e11ab2c847029281961813c0462da217fda5
longxiangam <longxiangam@163.com>
2024-06-15 17:20:07 +0800

    三针时钟盘面控件

--- src/widgets/clock_widget.rs
+++ src/widgets/clock_widget.rs
@@ -57,12 +57,9 @@ impl<C> Drawable for ClockWidget<C>
         // 绘制表盘边界
         let border_style = PrimitiveStyleBuilder::new()
             .stroke_color(self.front_color)
-            .stroke_width(2)
+            .stroke_width(1)
             .build();
 
-     /*   Rectangle::with_center(self.center, self.size)
-            .into_styled(border_style)
-            .draw(display)?;*/
 
 
         // 计算表盘的半径和中心点
@@ -74,6 +71,27 @@ impl<C> Drawable for ClockWidget<C>
         Circle::with_center(self.center, radius*2)
             .into_styled(border_style)
             .draw(display);
+        // 绘制刻度
+        for i in 0..60 {
+            let angle = 2.0 * PI * i as f32 / 60.0;
+            let inner_radius = if i % 5 == 0 {
+                radius as f32 * 0.85
+            } else {
+                radius as f32 * 0.95
+            };
+            let outer_radius = radius as f32;
+            let inner_point = center + Point::new(
+                (inner_radius * angle.cos()) as i32,
+                (inner_radius * angle.sin()) as i32,
+            );
+            let outer_point = center + Point::new(
+                (outer_radius * angle.cos()) as i32,
+                (outer_radius * angle.sin()) as i32,
+            );
+            Line::new(inner_point, outer_point)
+                .into_styled(border_style)
+                .draw(display)?;
+        }
 
 
         // 获取当前时间的小时、分钟和秒
@@ -82,9 +100,10 @@ impl<C> Drawable for ClockWidget<C>
         let seconds = self.datetime.second();
 
         // 计算每个指针的角度
-        let hour_angle = 2.0 * PI * (hours as f32 + minutes as f32 / 60.0) / 12.0;
-        let minute_angle = 2.0 * PI * (minutes as f32 + seconds as f32 / 60.0) / 60.0;
-        let second_angle = 2.0 * PI * seconds as f32 / 60.0;
+        // 整圆弧度为 2*pi ，除对应分隔数量得到步进弧度，  -PI/2 是因为时间是从12点位置开始计算，需要减去1/4 个圆弧度
+        let hour_angle = 2.0 * PI * (hours as f32 + minutes as f32 / 60.0) / 12.0 - PI / 2.0;
+        let minute_angle = 2.0 * PI * (minutes as f32 + seconds as f32 / 60.0) / 60.0 - PI / 2.0;
+        let second_angle = 2.0 * PI * seconds as f32 / 60.0 - PI / 2.0;
 
         // 定义指针的长度
         let hour_hand_length = radius as f32 * 0.5;
@@ -92,7 +111,17 @@ impl<C> Drawable for ClockWidget<C>
         let second_hand_length = radius as f32 * 0.9;
 
         // 定义指针的样式
-        let hand_style = PrimitiveStyleBuilder::new()
+        let hour_hand_style = PrimitiveStyleBuilder::new()
+            .stroke_color(self.front_color)
+            .stroke_width(3)
+            .build();
+
+        let minute_hand_style = PrimitiveStyleBuilder::new()
+            .stroke_color(self.front_color)
+            .stroke_width(2)
+            .build();
+
+        let second_hand_style = PrimitiveStyleBuilder::new()
             .stroke_color(self.front_color)
             .stroke_width(1)
             .build();
@@ -103,7 +132,7 @@ impl<C> Drawable for ClockWidget<C>
             (hour_hand_length * hour_angle.sin()) as i32,
         );
         Line::new(center, hour_hand_end)
-            .into_styled(hand_style.clone())
+            .into_styled(hour_hand_style)
             .draw(display)?;
 
         // 计算并绘制分针
@@ -112,7 +141,7 @@ impl<C> Drawable for ClockWidget<C>
             (minute_hand_length * minute_angle.sin()) as i32,
         );
         Line::new(center, minute_hand_end)
-            .into_styled(hand_style.clone())
+            .into_styled(minute_hand_style)
             .draw(display)?;
 
         // 计算并绘制秒针
@@ -121,7 +150,7 @@ impl<C> Drawable for ClockWidget<C>
             (second_hand_length * second_angle.sin()) as i32,
         );
         Line::new(center, second_hand_end)
-            .into_styled(hand_style)
+            .into_styled(second_hand_style)
             .draw(display)?;
 
 
